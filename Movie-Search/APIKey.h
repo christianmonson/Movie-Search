c3c22054005a7f0e0fa0cba6e0605ff99335deb8
Christian Monson <christianmonson@Christians-MacBook-Pro.local>
2015-03-06 14:28:14 -0700

    adds an APIKey class containing the APIKeyString

---
+++ Movie-Search/APIKey.h
@@ -0,0 +1,13 @@
+//
+//  APIKey.h
+//  Movie-Search
+//
+//  Created by Christian Monson on 3/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIKey : NSObject
+
+@end
---
+++ Movie-Search/APIKey.m
@@ -0,0 +1,13 @@
+//
+//  APIKey.m
+//  Movie-Search
+//
+//  Created by Christian Monson on 3/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "APIKey.h"
+
+@implementation APIKey
+
+@end
